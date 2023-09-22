8f0ffd963b423eed9bbd84b17b64b48831a904b2
ali <7731.aa@gmail.com>
2023-09-22 21:06:39 +0330

    Build Navbar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Grid, GridItem } from '@chakra-ui/react'
 import { useState } from 'react'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div>navbar</div>
+   <Grid templateAreas={{
+    base: `"nav" "main"`,
+    lg: `"nav nav" "aside main"`
+   }}>
+      <GridItem area="nav"><NavBar/></GridItem>
+      <GridItem area="main">main</GridItem>
+   </Grid>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import {
+  Avatar,
+  AvatarGroup,
+  HStack,
+  Heading,
+  Box,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import React from "react";
+import { AiOutlineUser } from "react-icons/ai";
+import { BsFillMoonFill, BsSearch } from "react-icons/bs";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <HStack
+      justifyContent="space-between"
+      paddingX={4}
+      paddingY={2}
+      boxShadow="0px 0px 3px gray"
+    >
+      <Heading>Users</Heading>
+      <InputGroup marginX={8}>
+        <InputLeftElement pointerEvents="none">
+          <BsSearch color="gray.300" />
+        </InputLeftElement>
+        <Input type="tel" placeholder="Search..." borderRadius={25} />
+      </InputGroup>
+      <HStack>
+        <BsFillMoonFill fontSize="20px" />
+        <AvatarGroup
+          paddingLeft={4}
+          marginLeft={4}
+          spacing="1rem"
+          borderLeft="1px solid black"
+        >
+          <Avatar bg="teal.500" size="sm" />
+        </AvatarGroup>
+      </HStack>
+    </HStack>
+  );
+};
 
-export default NavBar
+export default NavBar;
