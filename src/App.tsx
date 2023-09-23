9ec0f9544cdea778e6e68bbb3460b73e52d008bd
Ali Akbari <7731.aa@gmail.com>
2023-09-23 16:19:38 +0330

    Build modal for add user

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import {
 	HStack,
 	Heading,
 	Show,
+	useDisclosure,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import NavBar from "./components/NavBar";
 import UserList, { User } from "./components/UserList";
 import PostGrid from "./components/PostGrid";
+import AddUserModal from "./components/AddUserModal";
 
 export interface PostQuery {
 	userId: number | null;
@@ -19,41 +21,47 @@ export interface PostQuery {
 
 function App() {
 	const [postQuery, setPostQuery] = useState<PostQuery>({} as PostQuery);
-  const [userSelected, setUserSelected] = useState<User>({} as User)
+	const [userSelected, setUserSelected] = useState<User>({} as User);
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	return (
-		<Grid
-			templateAreas={{
-				base: `"nav" "main"`,
-				sm: `"nav nav" "aside main"`,
-			}}
-		>
-			<GridItem area="nav">
-				<NavBar />
-			</GridItem>
-			<Show above="sm">
-				<GridItem area="aside">
-					<UserList
-						onSelectUser={(user) => {
-							setPostQuery({ ...postQuery, userId: user.id });
-              setUserSelected(user)
-						}}
-					/>
+		<>
+			<Grid
+				templateAreas={{
+					base: `"nav" "main"`,
+					sm: `"nav nav" "aside main"`,
+				}}
+			>
+				<GridItem area="nav">
+					<NavBar />
 				</GridItem>
-			</Show>
-			<GridItem area="main">
-				<HStack justify="space-between">
-					<Box padding={4}>
-						<Heading>{ "Posts: " + userSelected.name || "All Posts"}</Heading>
-					</Box>
-					<ButtonGroup padding={4}>
-						<Button>Add User</Button>
-						<Button>Add Post</Button>
-					</ButtonGroup>
-				</HStack>
-				<PostGrid postQuery={postQuery} />
-			</GridItem>
-		</Grid>
+				<Show above="sm">
+					<GridItem area="aside">
+						<UserList
+							onSelectUser={(user) => {
+								setPostQuery({ ...postQuery, userId: user.id });
+								setUserSelected(user);
+							}}
+						/>
+					</GridItem>
+				</Show>
+				<GridItem area="main">
+					<HStack justify="space-between">
+						<Box padding={4}>
+							<Heading>
+								{"Posts: " + userSelected.name || "All Posts"}
+							</Heading>
+						</Box>
+						<ButtonGroup padding={4}>
+							<Button onClick={onOpen}>Add User</Button>
+							<Button>Add Post</Button>
+						</ButtonGroup>
+					</HStack>
+					<PostGrid postQuery={postQuery} />
+				</GridItem>
+			</Grid>
+      <AddUserModal isOpen={isOpen} onClose={onClose} onOpen={onOpen}/>
+		</>
 	);
 }
 
---
+++ src/components/AddUserModal.tsx
@@ -0,0 +1,63 @@
+import {
+	useDisclosure,
+	Button,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	FormControl,
+	FormLabel,
+	Input,
+	ModalFooter,
+} from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+	isOpen: boolean;
+	onOpen: () => void;
+	onClose: () => void;
+}
+
+const AddUserModal = ({ isOpen, onOpen, onClose }: Props) => {
+	const userNameRef = React.useRef(null);
+	const finalRef = React.useRef(null);
+
+	return (
+		<>
+			<Modal
+				initialFocusRef={userNameRef}
+				finalFocusRef={finalRef}
+				isOpen={isOpen}
+				onClose={onClose}
+			>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader>Create your account</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody pb={6}>
+						<FormControl>
+							<FormLabel>User Name</FormLabel>
+							<Input ref={userNameRef} placeholder="User name" />
+						</FormControl>
+
+						<FormControl mt={4}>
+							<FormLabel>Email</FormLabel>
+							<Input placeholder="Last name" />
+						</FormControl>
+					</ModalBody>
+
+					<ModalFooter>
+						<Button colorScheme="blue" mr={3}>
+							Save
+						</Button>
+						<Button onClick={onClose}>Cancel</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	);
+};
+
+export default AddUserModal;
