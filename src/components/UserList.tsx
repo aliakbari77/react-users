affdc3e9a09c652dc8d8504f6b8f7b05bcd50390
ali <7731.aa@gmail.com>
2023-09-23 23:48:13 +0330

    Create hook for get users list

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,64 +1,49 @@
 import {
-	List,
-	ListItem,
-	ListIcon,
-	Button,
-	Heading,
-	Box,
+  List,
+  ListItem,
+  ListIcon,
+  Button,
+  Heading,
+  Box,
 } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { MdCheckCircle, MdSettings } from "react-icons/md";
 import { AiOutlineUser } from "react-icons/ai";
+import useUsers from "../hooks/useUsers";
 
 export interface User {
-	id: number;
-	name: string;
-	email: string;
+  id: number;
+  name: string;
+  email: string;
 }
 
 interface Props {
-	onSelectUser: (user: User) => void;
+  onSelectUser: (user: User) => void;
 }
 
 const UserList = ({ onSelectUser }: Props) => {
-	const [users, setUsers] = useState<User[]>([]);
-	const [isLoading, setLoading] = useState(false);
-	useEffect(() => {
-		setLoading(true);
-		axios
-			.get<User[]>("https://jsonplaceholder.typicode.com/users")
-			.then((req) => {
-				setUsers(req.data);
-				setLoading(false);
-			})
-			.catch((err) => {
-				console.log(err);
-				setLoading(false);
-			});
-	}, []);
-	return (
-		<>
-			<Box padding={4}>
-				<Heading marginBottom={4} fontSize="24px">
-					Users List
-				</Heading>
-				<List spacing={8}>
-					{users.map((user) => (
-						<ListItem key={user.id}>
-							<ListIcon as={AiOutlineUser} fontSize="24px" />
-							<Button
-								variant="link"
-								onClick={() => onSelectUser(user)}
-							>
-								{user.name}
-							</Button>
-						</ListItem>
-					))}
-				</List>
-			</Box>
-		</>
-	);
+  const { data: users, isLoading, error } = useUsers();
+
+  return (
+    <>
+      <Box padding={4}>
+        <Heading marginBottom={4} fontSize="24px">
+          Users List
+        </Heading>
+        <List spacing={8}>
+          {users.map((user) => (
+            <ListItem key={user.id}>
+              <ListIcon as={AiOutlineUser} fontSize="24px" />
+              <Button variant="link" onClick={() => onSelectUser(user)}>
+                {user.name}
+              </Button>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </>
+  );
 };
 
 export default UserList;
---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,11 @@
+import useData from "./useData";
+
+export interface User {
+	id: number;
+	name: string;
+	email: string;
+}
+
+const useUsers = () => useData<User>("/users")
+
+export default useUsers
