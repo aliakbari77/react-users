4d13fa8ae552428e1bb23ec91fa8c66df5c4b1e3
ali <7731.aa@gmail.com>
2023-09-24 11:44:23 +0330

    Build form at add modal component

--- src/components/AddUserModal.tsx
+++ src/components/AddUserModal.tsx
@@ -1,63 +1,101 @@
 import {
-	useDisclosure,
-	Button,
-	Modal,
-	ModalOverlay,
-	ModalContent,
-	ModalHeader,
-	ModalCloseButton,
-	ModalBody,
-	FormControl,
-	FormLabel,
-	Input,
-	ModalFooter,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  FormControl,
+  FormLabel,
+  Input,
+  ModalFooter,
+  FormErrorMessage,
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
 } from "@chakra-ui/react";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  userName: z.string().min(3),
+  email: z.string().email(),
+});
+
+type FormData = z.infer<typeof schema>;
 
 interface Props {
-	isOpen: boolean;
-	onOpen: () => void;
-	onClose: () => void;
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
 }
 
 const AddUserModal = ({ isOpen, onOpen, onClose }: Props) => {
-	const userNameRef = React.useRef(null);
-	const finalRef = React.useRef(null);
+  const userNameRef = React.useRef(null);
+  const finalRef = React.useRef(null);
 
-	return (
-		<>
-			<Modal
-				initialFocusRef={userNameRef}
-				finalFocusRef={finalRef}
-				isOpen={isOpen}
-				onClose={onClose}
-			>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader>Add User</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody pb={6}>
-						<FormControl>
-							<FormLabel>User Name</FormLabel>
-							<Input ref={userNameRef} placeholder="User name" />
-						</FormControl>
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
-						<FormControl mt={4}>
-							<FormLabel>Email</FormLabel>
-							<Input placeholder="Last name" />
-						</FormControl>
-					</ModalBody>
+  const onSubmit = (data: any) => {
+    console.log(data);
+    onClose();
+  };
 
-					<ModalFooter>
-						<Button colorScheme="blue" mr={3} onClick={onClose}>
-							Save
-						</Button>
-						<Button onClick={onClose}>Cancel</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
-		</>
-	);
+  return (
+    <>
+      <Modal
+        initialFocusRef={userNameRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add User</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormControl>
+                <FormLabel>User Name</FormLabel>
+                <Input {...register("userName")} placeholder="User name" />
+                {errors.userName && (
+                  <Alert marginTop={2} borderRadius={4} status="error">
+                    <AlertTitle>{errors.userName.message}</AlertTitle>
+                  </Alert>
+                )}
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Email</FormLabel>
+                <Input {...register("email")} placeholder="Last name" />
+                {errors.email && (
+                  <Alert marginTop={2} borderRadius={4} status="error">
+                    <AlertTitle>{errors.email.message}</AlertTitle>
+                  </Alert>
+                )}
+              </FormControl>
+              <ModalFooter>
+                <Button type="submit" colorScheme="blue" mr={3}>
+                  Save
+                </Button>
+                <Button onClick={onClose}>Cancel</Button>
+              </ModalFooter>
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default AddUserModal;
