1cb294ec97f67c00caa3ad84851b35d64124fac2
ali <7731.aa@gmail.com>
2023-09-25 21:39:52 +0330

    Fix posts heading

--- src/components/PostHeading.tsx
+++ src/components/PostHeading.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import { User } from "./UserList";
 
 interface Props {
-	user: User;
+  user: User;
 }
 
 const PostHeading = ({ user }: Props) => {
-    const heading = user.name ? `Posts: ${user.name || ""}` : "Posts"
-	return <Heading>{heading}</Heading>;
+  const heading = user.name ? `Posts: ${user.name || ""}` : "All Posts";
+  return <Heading>{heading}</Heading>;
 };
 
 export default PostHeading;
